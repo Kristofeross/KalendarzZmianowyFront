f84ca4cafc221eaaf7b7adc3651b33f74d1d12dd
PK <krzysztofpielot@gmail.com>
2023-11-20 12:37:54 +0100

    16| New dockerfile, corrected login/register, remove unnecessary things

--- src/components/loginSignup/LoginSingup.jsx
+++ src/components/loginSignup/LoginSingup.jsx
@@ -11,9 +11,8 @@ import password_icon from '../assets/password.png';
 const LoginSignup = () => {
 
     // Do state
-    const [message, setMessage] = useState(null);
+    const [message, setMessage] = useState("");
     const [isToken, setToken] = useState(null);
-    const [diffPass, setDiffPass] = useState(false);
     const [action, setAction] = useState("Logowanie");
     const [name, setName] = useState("");
     const [email, setEmail] = useState("");
@@ -23,7 +22,6 @@ const LoginSignup = () => {
 
     // Do pól formularza
     const handleChangeInputs = e => {
-        // console.log(e.target.value)
         switch (e.target.name) {
             case 'name':
                 setName(e.target.value);
@@ -50,7 +48,6 @@ const LoginSignup = () => {
         setEmail("");
         setPassword("");
         setRepeatedPass("");
-        setDiffPass(false);
     };
 
     // Do przycisku wysyłającego formularz
@@ -60,34 +57,35 @@ const LoginSignup = () => {
                 setMessage("Proszę wypełnić puste pola");
                 return;
             }
-            if (password === repeatedPass) {
-                const verificationEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
-                const verificationPassword = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{6,}$/.test(password);
-                if (!verificationEmail) {
-                    setMessage("Niepoprawny adres email");
-                    return;
-                }
-                if (!verificationPassword) {
-                    setMessage("Hasło musi zawierać przynajmniej szejść liter, w tym conajmniej jedną cyfrę oraz jedną duża literę");
-                    return;
-                }
-                const data = {
-                    username: name,
-                    mail: email,
-                    password: password,
-                };
-                try {
-                    const response = await axios.post("http://127.0.0.1:5000/api/register", data);
-                    console.log("Response:", response.data);
-                    setMessage("Poprawnie zarejestrowany");
-                } catch (error) {
-                    console.error("Error:", error);
-                    // Kod błędu 400
-                    setMessage("Podana nazwa użytkownika jest już zajęta");
-                }
-                console.log(data);
-            } else {
-                setDiffPass(true);
+            const verificationEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
+            const verificationPassword = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{6,}$/.test(password);
+            if (!verificationEmail) {
+                setMessage("Niepoprawny adres email");
+                return;
+            }
+            if (!verificationPassword) {
+                setMessage("Hasło musi zawierać przynajmniej szejść liter, w tym conajmniej jedną cyfrę oraz jedną duża literę");
+                return;
+            }
+            if ( !(password === repeatedPass) ) {
+                setMessage("Podane hasło muszą być takie same");
+                return;
+            }
+
+            const data = {
+                username: name,
+                mail: email,
+                password: password,
+            };
+            try {
+                const response = await axios.post("http://127.0.0.1:5000/api/register", data);
+                // console.log("Response:", response.data);
+                setAction("Logowanie");
+                setMessage("Poprawnie zarejestrowany");
+            } catch (error) {
+                console.error("Error:", error);
+                // Kod błędu 400
+                setMessage("Podana nazwa użytkownika jest już zajęta");
             }
         } else { // Logowanie
             if(!name || !password){
@@ -95,18 +93,12 @@ const LoginSignup = () => {
                 return;
             }
             const data = {
-                email: email,
                 username: name,
                 password: password,
             };
             try {
                 const response = await axios.post("http://127.0.0.1:5000/api/login", data);
                 console.log("Response:", response.data);    
-                // console.log("Response:", response.data.token);
-
-                // Za pomocą localStorage
-                // localStorage.setItem('token', response.data.token);
-                // const token = localStorage.getItem('token');
 
                 // Za pomocą sessionStorage
                 sessionStorage.setItem('token', response.data.token);
@@ -117,7 +109,6 @@ const LoginSignup = () => {
                 // Kod błędu 401
                 setMessage("Niepoprawny login lub hasło");
             }
-            console.log(data);
         }
     };
 
@@ -138,9 +129,6 @@ const LoginSignup = () => {
     useEffect(() => {
         navigateCallback();
     }, [navigateCallback]);
-
-    console.log( sessionStorage.getItem('token') );
-    // console.log(isLogged)
 
     return (
         <>
@@ -172,7 +160,6 @@ const LoginSignup = () => {
                             <input type="password" placeholder="Powtórz hasło" value={repeatedPass} onChange={handleChangeInputs} name="repeatedPass" />
                         </div>
                     )}
-                    { diffPass && action === "Rejestracja" ? <p className="red">Hasło oraz jego powtórzenie nie są takie same</p> : null }
                 </div>
                 {action === "Rejestracja" ? null : (
                     <div className="forgot-password">
